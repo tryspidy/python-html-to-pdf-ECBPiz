4331fe8761c24cddc59a98f3ff279c6e7cc81cc5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 04:00:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Shell
+pip install pdfkit
+sudo apt-get install wkhtmltopdf
+
+# Python
+import pdfkit 
+pdfkit.from_file('input.html','shaurya.pdf') # .from_url and .from_string also exist
+
+# Source: https://www.geeksforgeeks.org/python-convert-html-pdf/
